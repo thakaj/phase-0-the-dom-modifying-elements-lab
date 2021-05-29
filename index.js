89753b936eca6c8aee58814dc992c8237d06c51b
thakaj <taulant.hakaj@scranton.edu>
2021-05-29 12:51:51 -0400

    commit tree 4c872bc870f3e94e69250559170492341bfda1c4

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const removeNode = document.querySelector(`main`)
+removeNode.remove()
+
+const newHeader = document.createElement(`h1`);
+newHeader.id = `victory`
+newHeader.innerHTML = `T is the champion`
